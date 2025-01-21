623ba7342c64da83d688a1f7edb849d5bab2330e
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-01-21 20:11:37 +0200

    simplified implementation and made it loop infinitely until the user decides to exit

--- main.py
+++ main.py
@@ -1,66 +1,53 @@
-prompt: str = 'Generate the Fibonacci sequence up to: '
-user_input: str = input(prompt)
+from sys import exit
 
-if user_input.isnumeric():
-    n: int = int(user_input)
-    while n < 0:
-        user_input = input(prompt)
-        if user_input.isnumeric():
-            n = int(user_input)
-        else:
-            break
+print('Welcome to The Fibonacci Sequence Generator!')
 
-while not user_input.isnumeric():
-    user_input = input(prompt)
-    if user_input.isnumeric():
-        n: int = int(user_input)
-        while n < 0:
-            user_input = input(prompt)
-            if user_input.isnumeric():
-                n = int(user_input)
-            else:
-                break
+while True:
+    user_input: str = input('Generate the Fibonacci sequence up to: ')
 
-fib_num: int = 0
-count: int = 0
-fib_seq: int = [fib_num]
-flag: bool = False
-
-while n != 0:
-    if fib_num == 0:
-        fib_num += 1
-        fib_seq.append(fib_num)
-        count += 1
+    if user_input == 'exit':
+        print('Thanks for trying my program!')
+        exit()
+    if not user_input.isnumeric():
+        print('Enter a whole number!')
+        continue
     else:
-        fib_num = fib_seq[count] + fib_seq[count-1]
-        fib_seq.append(fib_num)
-        count += 1
-    
-    if n in fib_seq:
-        break
-    
-    if (fib_seq[-1] > n) and (len(fib_seq) > n):
-        flag = True
-        break
+        n: int = int(user_input)
+        fib_num: int = 0
+        count: int = 0
+        fib_seq: list[int] = [fib_num]
+        flag: bool = False
+        while n != 0:
+            if fib_num == 0:
+                fib_num += 1
+                fib_seq.append(fib_num)
+                count += 1
+            else:
+                fib_num = fib_seq[count] + fib_seq[count-1]
+                fib_seq.append(fib_num)
+                count += 1
+            if n in fib_seq:
+                break
+            if (fib_seq[-1] > n) and (len(fib_seq) > n):
+                flag = True
+                break
+            
+        if n in fib_seq:
+            output: str = f'The Fibonacci sequence up to {n} is '
+            for fib_num in fib_seq:
+                if fib_seq.index(fib_num) != len(fib_seq)-1:
+                    output += (str(fib_num) + ', ')
+                else:
+                    output += (str(fib_num) + '.')
+                    
+            print(output)
+        
+        if flag:
+            output: str = f'The Fibonacci sequence up to the {n}th term is '
+            for i in range(n):
+                if i != n-1:
+                    output += (str(fib_seq[i]) + ', ')
+                else:
+                    output += (str(fib_seq[i]) + '.')
 
-if n in fib_seq:
-    output: str = f'The Fibonacci sequence up to {n} is '
-    
-    for fib_num in fib_seq:
-        if fib_seq.index(fib_num) != len(fib_seq)-1:
-            output += (str(fib_num) + ', ')
-        else:
-            output += (str(fib_num) + '.')
-    
-    print(output)
-
-if flag:
-    output: str = f'The Fibonacci sequence up to the {n}th term is '
-    
-    for i in range(n):
-        if i != n-1:
-            output += (str(fib_seq[i]) + ', ')
-        else:
-            output += (str(fib_seq[i]) + '.')
-
-    print(output)
+            print(output)
