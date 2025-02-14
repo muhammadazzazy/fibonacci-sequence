c608b15016db6c14b6311dbf68998dd1ce06d58c
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-02-14 22:39:44 +0200

    added functionality allowing input to be entered as words

--- main.py
+++ main.py
@@ -1,58 +1,75 @@
 from sys import exit
+from word2number import w2n
 
 
 def main() -> None:
     print('Welcome to The Fibonacci Sequence Generator!')
 
+    error_message: str = 'Please enter a non-negative integer...'
+    exit_message: str = 'Exiting program...'
+
     while True:
-        user_input: str = input('Generate the Fibonacci sequence up to: ')
+        try:
+            user_input: str = input('Generate the Fibonacci sequence up to: ')
+            if user_input == 'exit':
+                print(exit_message)
+                exit()
 
-        if user_input == 'exit':
-            print('Thanks for trying my program!')
-            exit()
-        if not user_input.isnumeric():
-            print('Enter a whole number!')
+            if not user_input.isnumeric():
+                n: int = w2n.word_to_num(user_input)
+            else:
+                n: int = int(user_input)
+
+            if n < 0:
+                print(error_message)
+                continue
+
+        except ValueError:
+            print(error_message)
             continue
-        else:
-            n: int = int(user_input)
-            fib_num: int = 0
-            count: int = 0
-            fib_seq: list[int] = [fib_num]
-            flag: bool = False
-            while n != 0:
-                if fib_num == 0:
-                    fib_num += 1
-                    fib_seq.append(fib_num)
-                    count += 1
-                else:
-                    fib_num = fib_seq[count] + fib_seq[count-1]
-                    fib_seq.append(fib_num)
-                    count += 1
-                if n in fib_seq:
-                    break
-                if (fib_seq[-1] > n) and (len(fib_seq) > n):
-                    flag = True
-                    break
 
+        except KeyboardInterrupt:
+            print(exit_message)
+            exit()
+
+        fib_num: int = 0
+        count: int = 0
+        fib_seq: list[int] = [fib_num]
+        flag: bool = False
+        while n != 0:
+            if fib_num == 0:
+                fib_num += 1
+                fib_seq.append(fib_num)
+                count += 1
+            else:
+                fib_num = fib_seq[count] + fib_seq[count-1]
+                fib_seq.append(fib_num)
+                count += 1
             if n in fib_seq:
-                output: str = f'The Fibonacci sequence up to {n} is '
-                for fib_num in fib_seq:
-                    if fib_seq.index(fib_num) != len(fib_seq)-1:
-                        output += (str(fib_num) + ', ')
-                    else:
-                        output += (str(fib_num) + '.')
+                break
+            if (fib_seq[-1] > n) and (len(fib_seq) > n):
+                flag = True
+                break
 
-                print(output)
+        if n in fib_seq:
+            output: str = f'The Fibonacci sequence up to {n} is '
+            for fib_num in fib_seq:
+                if fib_seq.index(fib_num) != len(fib_seq)-1:
+                    output += (str(fib_num) + ', ')
+                else:
+                    output += (str(fib_num) + '.')
 
-            if flag:
-                output: str = f'The Fibonacci sequence up to the {
-                    n}th term is '
-                for i in range(n):
-                    if i != n-1:
-                        output += (str(fib_seq[i]) + ', ')
-                    else:
-                        output += (str(fib_seq[i]) + '.')
-                print(output)
+            print(output)
+
+        if flag:
+            output: str = f'The Fibonacci sequence up to the {n}th term is '
+            for i in range(n):
+                if i != n-1:
+                    output += (str(fib_seq[i]) + ', ')
+                else:
+                    output += (str(fib_seq[i]) + '.')
+
+            print(output)
 
 
 if __name__ == '__main__':
